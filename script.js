d849370f9d0a40a805c3b687aff59960af7dbf9c
Caduguillaume <45256665+Caduguillaume@users.noreply.github.com>
2025-01-22 11:51:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-// Définir la date de fin du compte à rebours
-var countdownDate = new Date("Feb 2, 2025 17:00:00").getTime();
+const year = new Date().getFullYear();
+const fourthOfJuly = new Date(year, 6,4).getTime();
+const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const month = new Date().getMonth();
 
-// Mettre à jour le compte à rebours toutes les secondes
-var x = setInterval(function() {
+// countdown
+let timer = setInterval(function() {
 
-    // Obtenir la date et l'heure actuelles
-    var now = new Date().getTime();
-    
-    // Calculer la différence de temps
-    var distance = countdownDate - now;
-    
-    // Calculer les jours, heures, minutes et secondes
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Afficher le compte à rebours dans l'élément HTML
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  // get today's date
+  const today = new Date().getTime();
 
-    // Si le compte à rebours a expiré, afficher le contenu final
-    if (distance < 0) {
-        clearInterval(x); // Arrêter le compte à rebours
-        document.getElementById("balloons").style.display = "none"; // Masquer les ballons
-        document.getElementById("timer").style.display = "none"; // Masquer le timer
-        document.getElementById("final-content").style.display = "block"; // Afficher le contenu final
-    }
+  // get the difference
+  let diff;
+  if(month > 6) {
+    diff = fourthOfJulyNextYear - today;
+  } else {
+    diff = fourthOfJuly - today;
+  }
+
+
+
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
 }, 1000);
